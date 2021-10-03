fc5be0034cd1e4af5cf207f6678587969c31f015
marcValentino <mvalentino@id.uff.br>
2021-10-03 20:11:53 -0300

    Remoçao da funcionalidade de busca cep do controller de usuario
    Controller de user continuava redirecionando a antiga funcionalidade de busca cep de user, que nao existe mais. Esse redirecionamento foi removido

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -31,9 +31,4 @@ export class UserController {
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
-
-  @Get('/cep/:cepNumber')
-  getCEPInfo(@Param('cepNumber') cepNumber : string){
-    return this.userService.getCEPInfo(cepNumber);
-  }
 }
